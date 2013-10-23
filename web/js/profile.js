7da49a7c7118604f779c9b4e46849421b3322648
krakissi <mike@krakissi.net>
2013-10-22 23:03:48 -0400

    Tab lolighting when not selected.
    Inactive tabs on the profile page are now grayed a bit.

--- web/js/profile.js
+++ web/js/profile.js
@@ -14,15 +14,22 @@ function profile_init(){
 
 		if(tab<profile_tabs.length)
 			set_visible(profile_tabs[tab]);
-	}
+		else set_visible(profile_tabs[0]);
+	} else set_visible(profile_tabs[0]);
 }
 
 function set_visible(d){
 	var l = profile_tabs.length;
+	var tabid;
+
 	for(var i=0; i<l; i++){
 		document.getElementById(profile_tabs[i]).style.display='none';
+		if(tabid = document.getElementById("tab_"+profile_tabs[i]))
+			tabid.style.backgroundColor="#e0e0e0";
 	}
 	document.getElementById(d).style.display='block';
+	if(tabid = document.getElementById("tab_"+d))
+		tabid.style.backgroundColor="#ffffff";
 }
 
 
