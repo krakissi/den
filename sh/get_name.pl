6547cb12c80bc827ef077a3189d78cd86d13141e
krakissi <mike@krakissi.net>
2013-12-14 17:20:03 -0500

    Reorganizing Profile Page
    Making space for the feed, which will be the central focus of the
    profile.

---
+++ sh/get_name.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+my $id_profile = $ARGV[0];
+if($id_profile eq ""){
+	my %queryvals;
+	my $buffer = $ENV{'QUERY_STRING'};
+	if(length($buffer)>0){
+		my @pairs=split(/[;&]/, $buffer);
+		foreach my $pair (@pairs){
+			my ($name, $value) = split(/=/, $pair);
+			$value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+			$queryvals{$name} = $value; 
+			chomp($queryvals{$name});
+		}
+	}
+
+	$id_profile=$queryvals{'id'};
+	if($id_profile eq ""){ exit 0 }
+}
+
+my $database="den.db";
+chomp(my $homepath = qx/mod_home den/);
+chdir($homepath) or die "Can't get home.";
+
+my $sql = qq/SELECT name, display FROM profiles WHERE id_profile='$id_profile'/;
+chomp(my $resp = qx/sqlite3 "$database" "$sql"/);
+
+my ($name, $display) = split('\|', $resp, 2);
+if($display eq ""){ printf "$name" } else { printf "$display" }
+
+exit 0
---
+++ sh/pagetitle.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+
+my $title = "Social Network";
+
+my %queryvals;
+my $buffer = $ENV{'QUERY_STRING'};
+if(length($buffer)>0){
+	my @pairs=split(/[;&]/, $buffer);
+	foreach my $pair (@pairs){
+		my ($name, $value) = split(/=/, $pair);
+		$value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+		$queryvals{$name} = $value; 
+		chomp($queryvals{$name});
+	}
+}
+
+my $id_profile = $queryvals{'id'};
+
+
+exit 0
